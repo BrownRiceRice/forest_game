771ae521be33f973aa9e629e9bfee3933c00ab05
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2016-12-23 22:47:17 -0600

    Added a few more tests for multiplication stuff

--- test/tests.cpp
+++ test/tests.cpp
@@ -24,12 +24,51 @@ TEST_CASE( "Vectors (float arrays) can work with arthmetic", "[SceneParams]" ) {
         }
     }
 
-    SECTION("adding 1s and 1s gets 1s") {
+    SECTION("adding 1s and 1s gets 2s") {
         float *sum = (float *)malloc(sizeof(float) * SP_Count);
         sp.vectAdd(oneVec, oneVec, sum);
         for (int i = 0; i < SP_Count; i++) {
             REQUIRE( sum[i] == 2.0f);
         }
+    }
 
+    SECTION("adding count and count gets doubles") {
+        float *sum = (float *)malloc(sizeof(float) * SP_Count);
+        sp.vectAdd(countVec, countVec, sum);
+        for (int i = 0; i < SP_Count; i++) {
+            REQUIRE( sum[i] == i * 2.0f);
+        }
+    }
+
+    SECTION("subtracting 1s and count starts at -1") {
+        float *diff = (float *)malloc(sizeof(float) * SP_Count);
+        sp.vectMinus(countVec, oneVec, diff);
+        for (int i = 0; i < SP_Count; i++) {
+            REQUIRE( diff[i] == i - 1.0f);
+        }
+    }
+
+    SECTION("multiplying 1s and count gets count") {
+        float *prod = (float *)malloc(sizeof(float) * SP_Count);
+        sp.vectProd(countVec, oneVec, prod);
+        for (int i = 0; i < SP_Count; i++) {
+            REQUIRE( prod[i] == i * 1.0f);
+        }
+    }
+
+    SECTION("multiplying 1s and count gets 0s.") {
+        float *prod = (float *)malloc(sizeof(float) * SP_Count);
+        sp.vectProd(countVec, zeroVec, prod);
+        for (int i = 0; i < SP_Count; i++) {
+            REQUIRE( prod[i] == 0.0f);
+        }
+    }
+
+    SECTION("multiplying count by 2 scalar gets doubles") {
+        float *prod = (float *)malloc(sizeof(float) * SP_Count);
+        sp.vectProdScalar(countVec, 2.0f, prod);
+        for (int i = 0; i < SP_Count; i++) {
+            REQUIRE( prod[i] == i * 2.0f);
+        }
     }
 }
