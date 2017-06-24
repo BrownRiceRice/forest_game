c38f57fc2e9ebea9d913d3a00976d11baa9aa846
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2017-06-24 17:46:30 -0500

    Can load fonts, but can't yet display them.

--- src/forestMain.cpp
+++ src/forestMain.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
+#include <string>
 
 #define GLEW_STATIC // Depending on how you built/installed GLEW, you may want to change this
 #include <GL/glew.h>
@@ -24,6 +26,56 @@ using namespace ParamWorld;
 
 GLFWwindow *window;
 
+// FreeType Helper struct (https://learnopengl.com/#!In-Practice/Text-Rendering)
+struct Character {
+    GLuint TextureID; // ID handle of the glyph texture.
+    glm::ivec2 Size; // Size of glyph
+    glm::ivec2 Bearing; // Offset from baseline to the left/top of glyph
+    GLuint Advance; // Offset to advance to the next glyph.
+};
+
+std::map<GLchar, Character> Characters;
+
+GLuint VAO, VBO;
+
+/**
+ * Renders a line of text.
+ */
+void RenderText(GLuint shaderProgramID, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color) {
+    glUseProgram(shaderProgramID);
+    glUniform3f(glGetUniformLocation(shaderProgramID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++) {
+        Character ch = Characters[*c];
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+
+        GLfloat vertices[6][4] = {
+            {xpos,     ypos + h, 0.0, 0.0},
+            {xpos,     ypos,     0.0, 1.0},
+            {xpos + w, ypos,     1.0, 1.0},
+            {xpos,     ypos + h, 0.0, 0.0},
+            {xpos + w, ypos,     0.0, 1.0},
+            {xpos + w, ypos + h, 1.0, 1.0}
+        };
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        x += (ch.Advance >> 6 ) * scale;
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+
 int main(void) {
     int windowWidth = 1024;
     int windowHeight = 768;
@@ -42,6 +94,19 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // For Mac stuff
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // new OpenGL
 
+    // Initialize Free Type.
+    
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft)) {
+        std::cout << "ERROR::FREETYPE: Could not init FreeTypeLibrary" << std::endl;
+    }
+    FT_Face face;
+    if (FT_New_Face(ft, "../fonts/arial.ttf", 0, &face)) {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    }
+    // TODO: Resize based on window size.
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    
   	// Open a window and create its OpenGL context
     window = glfwCreateWindow(windowWidth, windowHeight, "Forest" , NULL, NULL);
   	if(window == NULL) {
@@ -76,8 +141,52 @@ int main(void) {
     glEnable(GL_DEPTH_TEST);
     // Accept fragment if it closer to the camera than the former one
     glDepthFunc(GL_LESS);
+    
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); //Disable byte-alignment restriction
+    
+    for (GLubyte c = 0; c < 128; c++) {
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+            std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D( // Specifies a 2D texture image.
+            GL_TEXTURE_2D,  // Target
+            0, // level
+            GL_RED, // internal format
+            face->glyph->bitmap.width, 
+            face->glyph->bitmap.rows, 
+            0, // border
+            GL_RED, // format
+            GL_UNSIGNED_BYTE, // type 
+            face->glyph->bitmap.buffer // data
+        );
+        // St texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            static_cast<GLuint>(face->glyph->advance.x)
+        };
+        Characters.insert(std::pair<GLchar, Character>(c, character));
+    }
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+    
+
+    // REquired for Font fragment shader.
+    //glEnable(GL_BLEND);
+    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     GLuint programID = LoadShaders("SimpleVertexShader.glsl", "SimpleFragmentShader.glsl");
+    // Shaders for fonts.
+    GLuint fontID = LoadShaders("FontVertexShader.glsl", "FontFragmentShader.glsl");
 
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
@@ -110,14 +219,14 @@ int main(void) {
         #endif
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(programID);
 
+        glUseProgram(programID);
         player.updateCameraFromInputs(window);
         w.updateExploredSquares(window, player.getPosition(), player.horizontalAngle);
-        glm::mat4 ProjectionMatrix = player.getProjectionMatrix();
-        glm::mat4 ViewMatrix = player.getViewMatrix();
+        w.Render(player.getProjectionMatrix(), player.getPosition(), player.getDirection(), player.getUp());
 
-        w.Render(ProjectionMatrix, player.getPosition(), player.getDirection(), player.getUp());
+        // Render Text.
+        // TODO
 
         // Swap buffers
         glfwSwapBuffers(window);
